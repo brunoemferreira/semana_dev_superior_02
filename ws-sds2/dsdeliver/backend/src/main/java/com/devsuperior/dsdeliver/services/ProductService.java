d05d28e99e9f897c8520e3e6b18f9f9c90f5ca35
brunoemferreira <brunoemf@gmail.com>
2021-01-05 17:26:25 -0300

    Get /products

---
+++ ws-sds2/dsdeliver/backend/src/main/java/com/devsuperior/dsdeliver/services/ProductService.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsdeliver.dto.ProductDTO;
+import com.devsuperior.dsdeliver.entities.Product;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
+
+// a Notation @Service serve paara registrar o component ProductService pra poder fazer a injeçõa de dependência
+@Service
+public class ProductService {
+	
+	// o @Autowired é um container para injeçãao de dependência forneceido pelo framework 
+	@Autowired
+	public ProductRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<ProductDTO> findAll(){
+		List<Product> list = repository.findAllByOrderByNameAsc();
+		return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
+	}
+
+}
