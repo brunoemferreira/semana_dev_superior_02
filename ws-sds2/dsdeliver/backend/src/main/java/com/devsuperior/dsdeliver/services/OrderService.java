efaee093a45d9b33902aa553ae28627fa41e83bc
brunoemferreira <brunoemf@gmail.com>
2021-01-05 19:39:34 -0300

    POST /orders

--- ws-sds2/dsdeliver/backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ ws-sds2/dsdeliver/backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.dto.ProductDTO;
 import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.OrderStatus;
+import com.devsuperior.dsdeliver.entities.Product;
 import com.devsuperior.dsdeliver.repositories.OrderRepository;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
 
 // a Notation @Service serve paara registrar o component ProductService pra poder fazer a injeçõa de dependência
 @Service
@@ -19,10 +24,26 @@ public class OrderService {
 	@Autowired
 	public OrderRepository repository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll(){
 		List<Order> list = repository.findOrdersWithProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
 	}
 
+	@Transactional
+	public OrderDTO insert(OrderDTO dto){
+		Order order = new Order(null,dto.getAddress(),dto.getLatitude(),dto.getLongitude(),Instant.now(), OrderStatus.PENDING);
+		// Esse for serve para associar os produtos ao pedido
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+	 // Salva o pedido no banco de dados
+	 order = repository.save(order);
+	 return new OrderDTO(order);
+	}
+	
 }
