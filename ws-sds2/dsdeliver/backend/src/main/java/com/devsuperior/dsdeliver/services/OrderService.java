409cd6ad77f67114aa1a8fe3cb40e1df183821b6
brunoemferreira <brunoemf@gmail.com>
2021-01-05 19:14:01 -0300

    Get /orders

---
+++ ws-sds2/dsdeliver/backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.repositories.OrderRepository;
+
+// a Notation @Service serve paara registrar o component ProductService pra poder fazer a injeçõa de dependência
+@Service
+public class OrderService {
+	
+	// o @Autowired é um container para injeçãao de dependência forneceido pelo framework 
+	@Autowired
+	public OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll(){
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+
+}
